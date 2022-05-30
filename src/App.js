9b1fe409acfaac32dc046d0f8dc19424654ebaac
Mark Maksi <artistoflife74@gmail.com>
2022-05-30 15:43:00 +0300

    adding '/' main route with two nested routes, using Outlet and putting a navigation on top of everything

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import "./Categories.styles.scss";
-import Directory from "./components/Directory/Directory.component";
+import React from "react";
+import Home from "./routes/Home.component";
+import { Routes, Route } from "react-router-dom";
+import Navigation from "./routes/Navigation/Navigation.component";
+
+const Shop = () => {
+  return <h1>Shop</h1>
+}
 
 const App = () => {
-  return <Directory />;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
